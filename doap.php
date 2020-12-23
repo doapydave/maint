58edd2ab30c4356d237dde2677a9b81eaad98b14
David Menache <dave@doap.com>
2020-12-23 15:37:55 +0000

    agency loop

--- doap.php
+++ doap.php
@@ -3,21 +3,29 @@
 include('../inc/dbp.php');
 
 #check dbu and dbp are good
-echo "
+echo " \n\n Code located at: /mnt/efs/doap/maint \n";
 
-Code located at: /mnt/efs/doap/maint
-";
+echo "\n dbh, dbu, dbp and dbname set \n\n";
 
-echo "
-dbh, dbu, dbp and dbname set
+echo "Agency query: 
+	select * from motd where appType = 'agency' order by AgencyUserId asc \n\n";
 
-";
-
-$mysqli = new mysqli($dbh, $dbu, $dbp, $dbname);
+#set agency query
+$agencies = "select * from motd where appType = 'agency' order by AgencyUserId asc";
 
 #connect to mysql
-#list agencies
-#if arg given run query on each agency db
+$mysqli = new mysqli($dbh, $dbu, $dbp, $dbname);
+
+#fetch and list agency count and agency names.
+if ($queryResults = $mysqli->query($agencies)) {
+                echo "\n".mysqli_num_rows($queryResults)." agencies.\n";
+                while($row = mysqli_fetch_array($queryResults)) {
+			echo $row['AgencyUserId']."\n";
+                }
+                echo "\n";
+}
+
+#todo: if arg given run query on each agency db
 
 
 ?>
