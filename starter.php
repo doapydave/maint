28cbb221ef6c1f8ae7e13098429dc079086b34fb
Dave <dave@Davids-MacBook-Air.local>
2020-12-23 09:20:21 -0600

    cleanedup

--- starter.php
+++ starter.php
@@ -4,12 +4,16 @@ include('../inc/dbp.php');
 
 #check dbu and dbp are good
 echo "
+
 Code located at: /mnt/efs/doap/maint
 ";
 
 //echo "DBU: ".$dbu." ";
 //echo "DBP: ".$dbp." ";
-echo "dbu and dbp set
+echo "
+dbu and dbp set
+
+
 ";
 #connect to mysql
 #list agencies
