5dfc11d47ed271e290b16e46898d33bd279686b6
David Menache <dave@doap.com>
2020-12-23 17:23:59 +0000

    agencies file updated for html

--- agencies.php
+++ agencies.php
@@ -1,18 +1,27 @@
+<style>
+.cleanlinks { text-decoration:none; }
+</style>
+
 <?php
+$AgencyUserId = str_replace('.doap.com','',$_SERVER['SERVER_NAME']);
+//echo "Running on: ".$AgencyUserId."\n";
+echo "<h1>".$_SERVER['SERVER_NAME']."</h1>";
+echo "<h3>AgencyUserID: ".$AgencyUserId."</h3>";
+
 #fetch mysql pass
 include('../inc/dbp.php');
 $mysqli = new mysqli($dbh, $dbu, $dbp, $dbname);
 
-$agencies = "select * from motd where appType = 'agency' and featured = 1 order by AgencyUserId asc";
+$agenciesQuery = "select * from motd where appType = 'agency' and featured = 1 order by AgencyUserId asc";
 
 #fetch and list agency count and agency names.
 
-if ($queryResults = $mysqli->query($agencies)) {
-	echo "<h2>".mysqli_num_rows($queryResults)." agencies</h2>";
+if ($queryResults = $mysqli->query($agenciesQuery)) {
+	echo "<h2>".mysqli_num_rows($queryResults)." Doap agencies</h2>";
 	echo "<ol>";
                 while($row = mysqli_fetch_array($queryResults)) {
 			//echo $row['AgencyUserId']."\n";
-			echo "<li>".$row['AgencyUserId'].".doap.com</li>";
+			echo "<li><a class=cleanlinks href=https://".$row['AgencyUserId'].".doap.com/agencies.php>".$row['AgencyUserId'].".doap.com</a></li>";
 			$AgencyDb = $row['AgencyUserId'].".doap.com";
                 }
 	echo "</ol>";
