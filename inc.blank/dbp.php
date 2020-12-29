3583dcc6ee8531b7ebaa4c2957e54bc45aaddc52
David Menache <dave@doap.com>
2020-12-29 02:43:55 +0000

    added inc connect template

---
+++ inc.blank/dbp.php
@@ -0,0 +1,12 @@
+<?php
+#move this folder outside the  project up one level and add db connect info.
+#database hostname
+$dbh = "------";
+#database name
+$dbname = "------";
+#database username
+$dbu = "------";
+#database password
+$dbp = "------";
+?>
+
